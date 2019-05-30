257a8e448680524351d8d412441d5bdc5e6e6b0a
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-30 18:34:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,15 @@
 def new_hash
-
+  new_hash = {}
+  new_hash
 end
 
 def actor
-
+  {:name => ["Dwayne The Rock Johnson"]}
+actor
 end
 
 def monopoly
 	monopoly = {}
-
 end
 
 def monopoly_with_second_tier
